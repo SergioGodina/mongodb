1290efa29bc5705fc9f9eb745d7bb97c169bdf16
SergioGodina <a1121250032@utch.edu.mx>
2022-06-29 20:42:49 -0600

    cambios del puerto revertidos

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(80, () => {
-    console.log(`Server Started at ${80}`)
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
 })
 
 app.use('/api', logs)
